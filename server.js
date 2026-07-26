bd2fe15018d6789df05427593da3b0873044dc69
adwaitKundap <adwaittkundap07@gmail.com>
2026-07-27 03:11:21 +0530

    Update backend for deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://YOUR-VERCEL-APP.vercel.app'
+    'https://blood-donor-frontend-nine.vercel.app'
 ];
 
 app.use(cors({
